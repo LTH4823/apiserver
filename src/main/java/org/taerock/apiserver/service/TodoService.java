076fd2b357748ecd3beec9c1493137b7c26be77e
taehwan Lee <peter4823zx@naver.com>
2023-12-20 15:24:16 +0900

    서비스 (수정, 삭제) 추가, 서비스 등록 테스트

--- src/main/java/org/taerock/apiserver/service/TodoService.java
+++ src/main/java/org/taerock/apiserver/service/TodoService.java
@@ -9,6 +9,13 @@ public interface TodoService {
 
 	TodoDTO get(Long tno);
 
+	Long register(TodoDTO dto);
+
+	// 수정이나 삭제는 원래가 문제가 안 생기는게 맞는 기능은 void로 반환 보시는게 맞습니다.
+	void modify(TodoDTO dto);
+
+	void remove(Long tno);
+
 	// java.8 부터 default를 사용하여 인터페이스에서도 기능을 선언하여 실행 할 수 있습니다.
 	// modelMapper나 자동으로 mapping 해주는 도구를 이용해도 되지만 단순하거나 간략한 구조일 때는 아래의 구조를 쓰시는게 추후 처리가 편합니다.
 	default TodoDTO entityToDTO(Todo todo){
