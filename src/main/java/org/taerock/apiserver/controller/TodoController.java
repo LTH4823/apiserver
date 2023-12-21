251c53538cc88ab09eb693af93bd79e3bdfc4567
taehwan Lee <peter4823zx@naver.com>
2023-12-21 16:21:17 +0900

    날짜 데이터 formatter 지정, RestController 등록 추가

--- src/main/java/org/taerock/apiserver/controller/TodoController.java
+++ src/main/java/org/taerock/apiserver/controller/TodoController.java
@@ -2,14 +2,13 @@ package org.taerock.apiserver.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.taerock.apiserver.dto.PageRequestDTO;
 import org.taerock.apiserver.dto.PageResponseDTO;
 import org.taerock.apiserver.dto.TodoDTO;
 import org.taerock.apiserver.service.TodoService;
+
+import java.util.Map;
 
 @RestController
 @Log4j2
@@ -32,6 +31,16 @@ public class TodoController {
 		log.info("list..........." + pageRequestDTO);
 
 		return todoService.getList(pageRequestDTO);
+	}
+
+	@PostMapping("/")
+	public Map<String, Long>register(@RequestBody TodoDTO dto){
+
+		log.info("todoDTO: " + dto);
+
+		Long tno = todoService.register(dto);
+
+		return Map.of("TNO", tno);
 	}
 
 }
