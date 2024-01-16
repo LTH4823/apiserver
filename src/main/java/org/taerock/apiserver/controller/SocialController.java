a3bdef9b73ba955fc2f31992ec1bb57010711f87
DESKTOP-5UD0QER\OSComPany <peter4823zx@naver.com>
2024-01-16 10:14:53 +0900

    API 서버 카카오 로그인 정보 리액트로 토큰 발급 처리

--- src/main/java/org/taerock/apiserver/controller/SocialController.java
+++ src/main/java/org/taerock/apiserver/controller/SocialController.java
@@ -4,7 +4,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.taerock.apiserver.dto.MemberDTO;
 import org.taerock.apiserver.service.MemberService;
+import org.taerock.apiserver.util.JWTUtil;
+
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @Log4j2
@@ -14,13 +19,21 @@ public class SocialController {
     private final MemberService memberService;
 
     @GetMapping("/api/member/kakao")
-    public String[] getMmemberFromKakako(String accessToken){
+    public Map<String, Object> getMmemberFromKakako(String accessToken){
 
         log.info("accessToken: " + accessToken);
 
-        memberService.getKakaoMember(accessToken);
+        MemberDTO memberDTO = memberService.getKakaoMember(accessToken);
 
-        return new String[]{"AAA", "BBB", "CCC"};
+        Map<String, Object> claims = memberDTO.getClaims();
+
+        String jwtAccessToken = JWTUtil.generateToken(claims, 10);
+        String jwtRefreshToken = JWTUtil.generateToken(claims, 60*24);
+
+        claims.put("accessToken", jwtAccessToken);
+        claims.put("refreshToken", jwtRefreshToken);
+
+        return claims;
     }
 
 }
