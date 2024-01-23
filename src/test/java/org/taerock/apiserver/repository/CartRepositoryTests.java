d6143f46aa55b3e88eee977bdbf4d367b3478a6a
LTH4823 <peter4823zx@naver.com>
2024-01-23 22:28:13 +0900

    장바구니 Repository 기능 추가 및 테스트

--- src/test/java/org/taerock/apiserver/repository/CartRepositoryTests.java
+++ src/test/java/org/taerock/apiserver/repository/CartRepositoryTests.java
@@ -4,6 +4,14 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+import org.taerock.apiserver.domain.Cart;
+import org.taerock.apiserver.domain.CartItem;
+import org.taerock.apiserver.domain.Member;
+import org.taerock.apiserver.domain.Product;
+
+import java.util.Optional;
 
 @SpringBootTest
 @Log4j2
@@ -15,6 +23,50 @@ public class CartRepositoryTests {
     @Autowired
     private CartItemRepository cartItemRepository;
 
+    @Transactional
+    @Commit
+    @Test
+    public void testInsertByProduct(){
+        String email = "user1@aaa.com";
+        Long pno = 3L;
+        int qty = 1;
+
+        // 이메일 상품번호로 장바구니 아이템 확인 없으면 추가 있으면 수정 변경해서 저장
+
+        CartItem cartItem = cartItemRepository.getItemOfPno(email, pno);
+
+        // 이미 사용자의 장바구니에 담겨져있는 경우
+        if(cartItem != null){
+            cartItem.changeQty(qty);
+            cartItemRepository.save(cartItem);
+            return;
+        }
+
+        // 사용자의 장바구니에 장바구니에 아이템을 만들어서 저장
+        // 장바구니 자체가 없는 경우
+        Optional<Cart> result = cartRepository.getCartOfMember(email);
+
+        Cart cart = null;
+
+        if(result.isEmpty()){
+            Member member = Member.builder().email(email).build();
+            Cart tempCart = Cart.builder().owner(member).build();
+
+            cartRepository.save(tempCart);
+
+        }else{ // 장바구니는 있으나 해당 상품의 장바구니 아이템은 없는 경우
+
+            cart = result.get();
+
+        }
+
+        Product product = Product.builder().pno(pno).build();
+        cartItem = CartItem.builder().cart(cart).product(product).qty(qty).build();
+
+        cartItemRepository.save(cartItem);
+
+    }
+
     @Test
     public void testListOfMember(){
 
@@ -23,5 +75,7 @@ public class CartRepositoryTests {
         cartItemRepository.getItemsOfCartDTOByEmail(email);
 
     }
+
+
 
 }
